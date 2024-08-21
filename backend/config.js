0060ff61b5d9f3c97e518cdcac4847af16766083
manasi-mishra <57686018+manasi-mishra@users.noreply.github.com>
2024-08-21 17:26:38 -0400

    Lesson 01: Create Node.js project from scratch  Lesson 02: Create our first Http Route  Lesson 03: Add MongoDB and mongoose to node js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,5 @@
-export const PORT = 5555;
+export const PORT = 5555
+
+export const mongoDBURL = 
+    'mongodb+srv://root:root@bookstore-mern.xg4ci.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore-MERN';
+
