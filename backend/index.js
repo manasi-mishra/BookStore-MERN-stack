021476b1808849d2ad650b0765174af35d75803e
manasi-mishra <57686018+manasi-mishra@users.noreply.github.com>
2024-08-21 17:44:30 -0400

    saving a new book using mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { PORT, mongoDBURL } from "./config.js";
+import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
 
 const app = express();
 
@@ -9,6 +10,27 @@ app.get('/', (request, response) => {
     return response.status(234).send('Welcome to MERN Stack Tutorial');
 });
  
+// route for saving a new book
+
+app.post('/books', async (request,response) => {
+    try {
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+
 mongoose 
     .connect(mongoDBURL)
     .then(() => {
