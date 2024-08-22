2743859e9ec0da31efee0de926f0dcd20ed66352
manasi-mishra <57686018+manasi-mishra@users.noreply.github.com>
2024-08-22 16:03:25 -0400

    Get all books with mongoose

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ import { Book } from './models/bookModel.js';
 
 const app = express();
 
+// middleware for parsing request body 
+app.use(express.json());
+
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send('Welcome to MERN Stack Tutorial');
@@ -12,8 +15,8 @@ app.get('/', (request, response) => {
  
 // route for saving a new book
 
-app.post('/books', async (request,response) => {
-    try {
+app.post('/books', async (request, response) => {
+    try { 
         if(
             !request.body.title ||
             !request.body.author ||
@@ -23,13 +26,35 @@ app.post('/books', async (request,response) => {
                 message: 'Send all required fields: title, author, publishYear',
             });
         }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
         
-    } catch(error){
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+
+    } catch (error) {
         console.log(error.message);
         response.status(500).send({message: error.message});
     }
 });
 
+// route to get ALL books from database 
+
+app.get('/books', async (request, response) => {
+    try{
+        const books = await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+});
 
 mongoose 
     .connect(mongoDBURL)
