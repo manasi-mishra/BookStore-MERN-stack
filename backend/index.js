c8a4e771eb674c955782f82d2c9bdde39f2050ab
manasi-mishra <57686018+manasi-mishra@users.noreply.github.com>
2024-08-23 12:48:52 -0400

    route to get one book from database by ID, updated README file

--- backend/index.js
+++ backend/index.js
@@ -56,6 +56,19 @@ app.get('/books', async (request, response) => {
     }
 });
 
+// route to get one book from database by ID
+
+app.get('/books/:id', async (request, response) => {
+    try{
+        const { id } = request.params;
+        const book = await Book.findById(id);
+        return response.status(200).json(book);
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+});
+
 mongoose 
     .connect(mongoDBURL)
     .then(() => {
---
+++ frontend/README.md
@@ -0,0 +1,2 @@
+Learning how to use MERN stack through this sample project. Following along this video here to learn:
+https://youtu.be/-42K44A1oMA?si=04ObBW8p7c1tlxRU
